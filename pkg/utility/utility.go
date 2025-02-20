8198c0f4ad98af09589efe6605d652895228438a
mplattu <matti.lattu@iki.fi>
2025-02-20 21:09:43 +0200

    Set environment variables in .env file
    Since the binary is called using CGI protocol the current working directory
    (cwd) can vary. Because of this, the .env file is searched from the
    same directory as the executable itself.

--- pkg/utility/utility.go
+++ pkg/utility/utility.go
@@ -1,6 +1,10 @@
 package utility
 
-import "os"
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func FileExists(filepath string) bool {
 	_, err := os.Stat(filepath)
@@ -12,4 +16,14 @@ func FileIsWriteable(filepath string) bool {
 	_, err := os.OpenFile(filepath, os.O_RDWR, 0666)
 
 	return err == nil
+}
+
+func GetDotEnvPath() string {
+	executable, err := os.Executable()
+	if err != nil {
+		log.Printf("could not get path of the executable script: %s", err)
+		return ".env"
+	}
+
+	return filepath.Join(filepath.Dir(executable), ".env")
 }
